79525987bda83535ff0d464a8b91985b48681b66
hs.yang <didgsutmd1@ajou.ac.kr>
2022-11-21 15:03:05 +0900

    [TEST] test commit

--- src/main/java/com/konai/kurong/faketee/FaketeeApplication.java
+++ src/main/java/com/konai/kurong/faketee/FaketeeApplication.java
@@ -8,6 +8,7 @@ public class FaketeeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FaketeeApplication.class, args);
+        System.out.println("test log");
     }
 
 }
