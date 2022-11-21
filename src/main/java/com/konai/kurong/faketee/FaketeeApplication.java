b18ecccec0928996f26bbbacaae52025c826b4fb
hyunseung yang <dev.hsyang@gmail.com>
2022-11-21 14:26:26 +0900

    [ADD]create project

---
+++ src/main/java/com/konai/kurong/faketee/FaketeeApplication.java
@@ -0,0 +1,13 @@
+package com.konai.kurong.faketee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaketeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaketeeApplication.class, args);
+    }
+
+}
