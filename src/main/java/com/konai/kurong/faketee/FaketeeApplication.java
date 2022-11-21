514bb848e22d555a7bd94fdc433d6dea55b799de
hyunseung yang <dev.hsyang@gmail.com>
2022-11-21 15:06:30 +0900

    [TEST] test commit 2

--- src/main/java/com/konai/kurong/faketee/FaketeeApplication.java
+++ src/main/java/com/konai/kurong/faketee/FaketeeApplication.java
@@ -9,6 +9,7 @@ public class FaketeeApplication {
     public static void main(String[] args) {
         SpringApplication.run(FaketeeApplication.class, args);
         System.out.println("test log");
+        System.out.println("test log2");
     }
 
 }
