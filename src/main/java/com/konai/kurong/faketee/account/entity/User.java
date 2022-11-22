6503088c51b0249c7c37ce3495dd429c118e8846
hyunseung yang <dev.hsyang@gmail.com>
2022-11-22 12:53:24 +0900

    [ADD] 템플릿 추가

--- src/main/java/com/konai/kurong/faketee/account/entity/User.java
+++ src/main/java/com/konai/kurong/faketee/account/entity/User.java
@@ -1,4 +1,10 @@
 package com.konai.kurong.faketee.account.entity;
 
+import java.time.LocalDateTime;
+
 public class User {
+    private LocalDateTime CreatedDateTime;
+    private LocalDateTime UpdatedDateTime;
+    private String CreatedId;
+    private String UpdatedId;
 }
