4856b69dee63237c8b2ea42a38cd8dda48046450
hyunseung yang <dev.hsyang@gmail.com>
2022-11-22 15:53:33 +0900

    [ADD] user service methods / [FIXED] user entity, user dto

--- src/main/java/com/konai/kurong/faketee/account/dto/UserUpdateRequestDto.java
+++ src/main/java/com/konai/kurong/faketee/account/dto/UserUpdateRequestDto.java
@@ -1,13 +1,20 @@
 package com.konai.kurong.faketee.account.dto;
 
 import com.konai.kurong.faketee.account.entity.User;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Builder
+@Getter
+@AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class UserUpdateRequestDto {
 
     private String oldPassword;
     private String newPassword;
+
+    public void setEncPassword(String encPassword){
+
+        this.newPassword = encPassword;
+    }
 }
