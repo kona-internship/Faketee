61fa0861046f9f0d245d9ab1e1ee4d2785e0bc99
sh_park <shparkso162@gmail.com>
2022-11-23 17:45:06 +0900

    [ADD] location html, js, entity

--- src/main/java/com/konai/kurong/faketee/corporation/controller/CorporationApiController.java
+++ src/main/java/com/konai/kurong/faketee/corporation/controller/CorporationApiController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
+
 @RequiredArgsConstructor
 @RestController
 @Slf4j
@@ -15,7 +17,7 @@ public class CorporationApiController {
     private final CorporationService corporationService;
 
     @PostMapping("/api/corporation")
-    public Long register(@RequestBody CorporationSaveRequestDto requestDto) {
+    public Long register(@Valid @RequestBody CorporationSaveRequestDto requestDto) {
 
         Long id = corporationService.registerCorporation(requestDto);
         return id;
