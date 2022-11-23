92380985155a702c553e276a100b0c2cabc11e62
hyunseung yang <dev.hsyang@gmail.com>
2022-11-23 13:46:27 +0900

    [ADD] mypage controller, API

--- src/main/java/com/konai/kurong/faketee/account/controller/MyPageApiController.java
+++ src/main/java/com/konai/kurong/faketee/account/controller/MyPageApiController.java
@@ -1,4 +1,27 @@
 package com.konai.kurong.faketee.account.controller;
 
+import com.konai.kurong.faketee.account.dto.UserUpdateRequestDto;
+import com.konai.kurong.faketee.account.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/account")
+@RequiredArgsConstructor
+@RestController
 public class MyPageApiController {
+
+    private final UserService userService;
+
+    @PostMapping("/update-password/{userID}")
+    public ResponseEntity<?> updatePassword(@PathVariable Long userID, @RequestBody UserUpdateRequestDto requestDto){
+
+        return ResponseEntity.ok(userService.updatePassword(userID, requestDto));
+    }
+
+    @PostMapping("/delete/{userID")
+    public void delete(@PathVariable Long userID){
+
+        userService.delete(userID);
+    }
 }
--- src/main/java/com/konai/kurong/faketee/account/controller/MyPageController.java
+++ src/main/java/com/konai/kurong/faketee/account/controller/MyPageController.java
@@ -2,10 +2,23 @@ package com.konai.kurong.faketee.account.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/account")
 @RequiredArgsConstructor
 @Controller
 public class MyPageController {
+
+    @GetMapping("/mypage")
+    public String mypage(){
+
+        return "account/mypage";
+    }
+
+    @GetMapping("/mypage/set-info")
+    public String setInfo(){
+
+        return "account/set-info";
+    }
 }
