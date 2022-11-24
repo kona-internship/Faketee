d4e879663ec1c3da4f90c571a3f9df7fe6ca0079
hs.yang <didgsutmd1@ajou.ac.kr>
2022-11-24 12:07:44 +0900

    [FIXED] base entity / [ADD] create id, last update id

--- src/main/java/com/konai/kurong/faketee/FaketeeApplication.java
+++ src/main/java/com/konai/kurong/faketee/FaketeeApplication.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class FaketeeApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(FaketeeApplication.class, args);
     }
 }
