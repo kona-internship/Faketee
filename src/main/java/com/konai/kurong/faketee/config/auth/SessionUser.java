605985f8a05b735126ab225e1d586b8aef51dae6
rnkwg <rnkwg@naver.com>
2022-11-24 12:07:57 +0900

    [ADD] js 추가

--- src/main/java/com/konai/kurong/faketee/config/auth/SessionUser.java
+++ src/main/java/com/konai/kurong/faketee/config/auth/SessionUser.java
@@ -1,5 +1,6 @@
 package com.konai.kurong.faketee.config.auth;
 
+import com.konai.kurong.faketee.account.entity.User;
 import com.konai.kurong.faketee.account.util.Role;
 import lombok.Getter;
 import org.springframework.security.core.SpringSecurityCoreVersion;
