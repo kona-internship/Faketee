faa1b1e580fc0b5d89087a1eca5a1cdf910943ac
hyunseung yang <dev.hsyang@gmail.com>
2022-11-24 13:57:57 +0900

    [REFACTOR] position
    직무 리펙토링

--- src/main/java/com/konai/kurong/faketee/position/entity/Position.java
+++ src/main/java/com/konai/kurong/faketee/position/entity/Position.java
@@ -24,7 +24,7 @@ import static javax.persistence.GenerationType.SEQUENCE;
         initialValue=1, //시작값
         allocationSize=1 //메모리를 통해 할당할 범위 사이즈
 )
-@Table(name = "Pos")
+@Table(name = "POS")
 public class Position {
 
     @Id
