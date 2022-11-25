57773e7c524451eecc98cb7128c279413757dcb1
hyunseung yang <dev.hsyang@gmail.com>
2022-11-25 13:32:50 +0900

    [ADD] web mvc configuration

---
+++ src/main/java/com/konai/kurong/faketee/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.konai.kurong.faketee.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    /**
+     * 정적 리소스 파일 읽어드리기 위해 핸들러 설정
+     * @param registry
+     */
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/templates/**")
+                .addResourceLocations("classpath:/templates/");
+    }
+}
