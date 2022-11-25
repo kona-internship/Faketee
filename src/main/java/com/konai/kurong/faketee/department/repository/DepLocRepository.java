e2c3370bad430b71220af6a026cdfce6f3206480
hyunseung yang <dev.hsyang@gmail.com>
2022-11-25 16:13:53 +0900

    [ADD] depLoc service

--- src/main/java/com/konai/kurong/faketee/department/repository/DepLocRepository.java
+++ src/main/java/com/konai/kurong/faketee/department/repository/DepLocRepository.java
@@ -5,9 +5,13 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface DepLocRepository extends JpaRepository<DepLoc, Long> {
 
     @EntityGraph(attributePaths = {"location", "department"})
     List<DepLoc> findAllByDepartment_Id(Long depId);
+
+    Optional<DepLoc> findDepLocByLocationId(Long locId);
+
 }
