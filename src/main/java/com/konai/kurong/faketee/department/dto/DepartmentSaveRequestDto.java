07c51337d7aaa6502219fbc18478de90657bd4da
hyunseung yang <dev.hsyang@gmail.com>
2022-11-25 17:54:22 +0900

    [FIXED] department request (number of loc)
    조직을 등록할 때 출퇴근 장소를 하나만 등록할 수 있던 것을 여러개 등록할 수 있도록 변경

--- src/main/java/com/konai/kurong/faketee/department/dto/DepartmentSaveRequestDto.java
+++ src/main/java/com/konai/kurong/faketee/department/dto/DepartmentSaveRequestDto.java
@@ -4,6 +4,7 @@ import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Data
 public class DepartmentSaveRequestDto {
@@ -12,7 +13,7 @@ public class DepartmentSaveRequestDto {
     private String name;
 
     private Long superId;
-
     @NotNull
-    private Long locationId;
+    private List<Long> locationIdList;
 }
+
