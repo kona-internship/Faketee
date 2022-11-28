ada291b95b736d79a142b65ccb184fe66756c5cb
hs.yang <didgsutmd1@ajou.ac.kr>
2022-11-28 17:20:27 +0900

    [ADD] custom login succes handler

--- src/main/java/com/konai/kurong/faketee/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/konai/kurong/faketee/config/auth/CustomOAuth2UserService.java
@@ -65,7 +65,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
                 of(registrationID, userNameAttributeName, oAuth2User.getAttributes());
 
         User user = saveOrUpdate(attributes);
-        httpSession.setAttribute("user", new SessionUser(user).getId());
+        //httpSession.setAttribute("user", new SessionUser(user).getId());
 
         return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRole().getKey())),
                 attributes.getAttributes(),
