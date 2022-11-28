92f26aca0a786f22e981c28199f54f75a909ee5b
hyunseung yang <dev.hsyang@gmail.com>
2022-11-28 17:26:06 +0900

    [FIXED] custom login success handler

--- src/main/java/com/konai/kurong/faketee/util/CustomLoginSuccessHandler.java
+++ src/main/java/com/konai/kurong/faketee/util/CustomLoginSuccessHandler.java
@@ -1,5 +1,6 @@
 package com.konai.kurong.faketee.util;
 
+import com.konai.kurong.faketee.account.entity.User;
 import com.konai.kurong.faketee.account.repository.UserRepository;
 import com.konai.kurong.faketee.config.auth.SessionUser;
 import com.konai.kurong.faketee.util.exception.NoUserFoundException;
@@ -23,11 +24,12 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
+        User loginUser = userRepository.findByEmail(authentication.getPrincipal().toString()).orElseThrow(() -> new NoUserFoundException());
 
         /**
          * 이메일 인증이 진행되지 않은 계정에 대해 block처리
          */
-        if (userRepository.findByEmail(authentication.getPrincipal().toString()).orElseThrow(() -> new NoUserFoundException()).getEmailAuthStatus().equals("F")){
+        if (loginUser.getEmailAuthStatus().equals("F")){
 
             response.sendRedirect("/account/home");
             return ;
@@ -38,8 +40,7 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
          * "user" attribute에 user 정보를 저장한다.
          * SessionUser를 이용하여 user 정보를 직렬화해서 저장
          */
-        request.getSession().setAttribute("user", new SessionUser(userRepository.findByEmail(authentication.getPrincipal().toString()).orElseThrow(() -> new NoUserFoundException())));
-        System.out.println(request.getSession().getAttribute("user"));
+        request.getSession().setAttribute("user", new SessionUser(loginUser));
         response.sendRedirect("/account/set-auth");
     }
 }
