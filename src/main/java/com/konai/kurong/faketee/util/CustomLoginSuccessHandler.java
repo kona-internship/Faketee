84d6836ea0ebe12f079d898cdb5417f176f998a0
hyunseung yang <dev.hsyang@gmail.com>
2022-11-28 17:20:27 +0900

    [ADD] custom login succes handler

--- src/main/java/com/konai/kurong/faketee/util/CustomLoginSuccessHandler.java
+++ src/main/java/com/konai/kurong/faketee/util/CustomLoginSuccessHandler.java
@@ -23,13 +23,23 @@ public class CustomLoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
+
+        /**
+         * 이메일 인증이 진행되지 않은 계정에 대해 block처리
+         */
+        if (userRepository.findByEmail(authentication.getPrincipal().toString()).orElseThrow(() -> new NoUserFoundException()).getEmailAuthStatus().equals("F")){
+
+            response.sendRedirect("/account/home");
+            return ;
+        }
+
         /**
          * 로그인 성공시, authentication.getPrincipal() (로그인한 email)로 user정보를 DB에서 find해와서
          * "user" attribute에 user 정보를 저장한다.
          * SessionUser를 이용하여 user 정보를 직렬화해서 저장
          */
         request.getSession().setAttribute("user", new SessionUser(userRepository.findByEmail(authentication.getPrincipal().toString()).orElseThrow(() -> new NoUserFoundException())));
-        //System.out.println(request.getSession().getAttribute("user"));
+        System.out.println(request.getSession().getAttribute("user"));
         response.sendRedirect("/account/set-auth");
     }
 }
