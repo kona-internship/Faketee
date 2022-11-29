b19edf1afd05d079fc4cfef1546a08956eb90773
hyunseung yang <dev.hsyang@gmail.com>
2022-11-29 09:30:20 +0900

    [ADD] google oauth2 complete, [REFACTOR] redirect package

--- src/main/java/com/konai/kurong/faketee/account/util/Type.java
+++ src/main/java/com/konai/kurong/faketee/account/util/Type.java
@@ -8,7 +8,8 @@ import lombok.RequiredArgsConstructor;
 public enum Type {
 
     GENERAL("TYPE_GENERAL", "일반"),
-    GOOGLE("TYPE_GOOGLE", "구글");
+    GOOGLE("TYPE_GOOGLE", "구글"),
+    NAVER("TYPE_NAVER", "네이버");
 
     private final String key;
     private final String title;
