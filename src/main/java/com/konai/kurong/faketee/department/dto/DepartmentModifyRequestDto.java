79928f43d6582b17a324214488088f1e89920974
hyunseung yang <dev.hsyang@gmail.com>
2022-11-29 13:14:48 +0900

    [FIXED] DepartmentModifyRequestDto (departmentIdList 추가)
    하위조직 id 목록 추가

--- src/main/java/com/konai/kurong/faketee/department/dto/DepartmentModifyRequestDto.java
+++ src/main/java/com/konai/kurong/faketee/department/dto/DepartmentModifyRequestDto.java
@@ -7,6 +7,7 @@ import java.util.List;
 @Data
 public class DepartmentModifyRequestDto {
     private String name;
+    private List<Long> lowDepartmentIdList;
     private List<Long> locationIdList;
     private Boolean isModifyLow;
 }
