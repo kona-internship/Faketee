4226a1502523d85cebdcbdacb46c95bafa366a33
hyunseung yang <dev.hsyang@gmail.com>
2022-11-29 10:33:13 +0900

    [ADD] department modify request dto

---
+++ src/main/java/com/konai/kurong/faketee/department/dto/DepartmentModifyRequestDto.java
@@ -0,0 +1,12 @@
+package com.konai.kurong.faketee.department.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class DepartmentModifyRequestDto {
+    private String name;
+    private List<Long> locationIdList;
+    private Boolean isModifyLow;
+}
