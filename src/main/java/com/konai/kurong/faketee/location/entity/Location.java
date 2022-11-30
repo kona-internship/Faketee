0ba98d41cc8db56fc6f5521b666aec0586d497b3
hyunseung yang <dev.hsyang@gmail.com>
2022-11-30 17:59:05 +0900

    [FIXED] extends base entity 수정/ 회사등록 html

--- src/main/java/com/konai/kurong/faketee/location/entity/Location.java
+++ src/main/java/com/konai/kurong/faketee/location/entity/Location.java
@@ -1,7 +1,7 @@
 package com.konai.kurong.faketee.location.entity;
 
 import com.konai.kurong.faketee.corporation.entity.Corporation;
-import com.konai.kurong.faketee.utils.jpa_auditing.BaseUserEntity;
+import com.konai.kurong.faketee.utils.jpa_auditing.BaseEntity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,8 +9,6 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.Date;
 
 @Entity
 @Getter
@@ -18,7 +16,7 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "LOC")
-public class Location extends BaseUserEntity {
+public class Location extends BaseEntity {
     @Id
     @SequenceGenerator(name = "LOC_ID_GENERATOR", sequenceName = "LOC_SEQUENCE", initialValue = 1, allocationSize = 1)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "LOC_ID_GENERATOR")
