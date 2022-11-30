f84ef4128b5c2cf9ae9ee14cf659ef7e1e27eddb
hyunseung yang <dev.hsyang@gmail.com>
2022-11-30 13:09:42 +0900

    [ADD] exception

---
+++ src/main/resources/static/js/utils/exception.js
@@ -0,0 +1,10 @@
+function matchId(jqXHR) {
+    let jsonValue = jQuery.parseJSON(jqXHR.responseText);
+    return jsonValue.message;
+}
+
+function showError(id){
+    if(id=="400_001_001") {
+        alert("하위조직이 존재하여 삭제할 수 없습니다.");
+    }
+}
