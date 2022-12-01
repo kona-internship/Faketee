94cec486783d3de313abe4a20b44e43d1cbae04c
hs.yang <didgsutmd1@ajou.ac.kr>
2022-12-01 13:24:12 +0900

    [ADD] template controller

--- src/main/java/com/konai/kurong/faketee/schedule/controller/TemplateController.java
+++ src/main/java/com/konai/kurong/faketee/schedule/controller/TemplateController.java
@@ -2,10 +2,23 @@ package com.konai.kurong.faketee.schedule.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
 @RequestMapping("/corporation/{cordId}/template")
 @Controller
 public class TemplateController {
+
+    @GetMapping("/list")
+    public String templateList(){
+
+        return "schedule/template-list";
+    }
+
+    @GetMapping("/new")
+    public String newTemplate(){
+
+        return "schedule/template-new";
+    }
 }
