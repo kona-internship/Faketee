20b0a385972a4dd3b1b5be19597f74daf4ccb714
hs.yang <didgsutmd1@ajou.ac.kr>
2022-12-01 13:19:40 +0900

    [ADD] template repo, service, controller, dto

---
+++ src/main/java/com/konai/kurong/faketee/schedule/controller/TemplateController.java
@@ -0,0 +1,11 @@
+package com.konai.kurong.faketee.schedule.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequiredArgsConstructor
+@RequestMapping("/corporation/{cordId}/template")
+@Controller
+public class TemplateController {
+}
---
+++ src/main/java/com/konai/kurong/faketee/schedule/service/TemplateService.java
@@ -0,0 +1,28 @@
+package com.konai.kurong.faketee.schedule.service;
+
+import com.konai.kurong.faketee.schedule.dto.TemplateSaveRequestDto;
+import com.konai.kurong.faketee.schedule.entity.Template;
+import com.konai.kurong.faketee.schedule.repository.TemplateRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class TemplateService {
+
+    private final TemplateRepository templateRepository;
+
+    @Transactional
+    public Long save(TemplateSaveRequestDto requestDto){
+
+        Template template = templateRepository.save(requestDto.toEntity());
+        return template.getId();
+    }
+
+    @Transactional
+    public void delete(Long id){
+
+        templateRepository.deleteById(id);
+    }
+}
