177256e6c83e0fa40fec47a62ca19e39997b7f73
hyunseung yang <dev.hsyang@gmail.com>
2022-12-02 09:10:03 +0900

    [REFACTOR] rename schedule type service

--- src/main/java/com/konai/kurong/faketee/schedule/controller/ScheduleApiController.java
+++ src/main/java/com/konai/kurong/faketee/schedule/controller/ScheduleApiController.java
@@ -1,8 +1,7 @@
 package com.konai.kurong.faketee.schedule.controller;
 
-import com.konai.kurong.faketee.position.dto.PositionSaveRequestDto;
 import com.konai.kurong.faketee.schedule.dto.ScheduleTypeSaveRequestDto;
-import com.konai.kurong.faketee.schedule.service.ScheduleService;
+import com.konai.kurong.faketee.schedule.service.ScheduleTypeService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -17,19 +16,19 @@ import javax.validation.Valid;
 @RequestMapping("/api/corporation/{corId}/sch")
 public class ScheduleApiController {
 
-    private final ScheduleService scheduleService;
+    private final ScheduleTypeService scheduleTypeService;
 
     @PostMapping("/type/list")
     public ResponseEntity<?> getSchTypeList(@PathVariable(name = "corId") Long corId) {
 
-        return new ResponseEntity<>(scheduleService.getSchTypeList(corId), HttpStatus.OK);
+        return new ResponseEntity<>(scheduleTypeService.getSchTypeList(corId), HttpStatus.OK);
     }
 
     @PostMapping("/type")
     public ResponseEntity<?> registerSchType(@PathVariable(name = "corId") Long corId,
                                              @Valid @RequestBody ScheduleTypeSaveRequestDto requestDto) {
 
-        scheduleService.registerSchType(corId, requestDto);
+        scheduleTypeService.registerSchType(corId, requestDto);
 
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
@@ -37,8 +36,8 @@ public class ScheduleApiController {
     @PostMapping("/type/delete/{typeId}")
     public ResponseEntity<?> removeSchType(@PathVariable(name = "corId") Long corId,
                                             @PathVariable(name = "typeId") Long typeId){
-        scheduleService.removeSchType(corId, typeId);
+        scheduleTypeService.removeSchType(corId, typeId);
 
-        return new ResponseEntity<>(scheduleService.getSchTypeList(corId), HttpStatus.OK);
+        return new ResponseEntity<>(scheduleTypeService.getSchTypeList(corId), HttpStatus.OK);
     }
 }
