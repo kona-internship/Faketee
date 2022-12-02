b935562063611d6176f42d48585bbc6ad6158899
Janghyunho977 <54993215+Janghyunho977@users.noreply.github.com>
2022-12-02 17:44:12 +0900

    [FIXED] emauth interceptor

--- src/main/java/com/konai/kurong/faketee/department/dto/DepartmentSaveRequestDto.java
+++ src/main/java/com/konai/kurong/faketee/department/dto/DepartmentSaveRequestDto.java
@@ -1,13 +1,17 @@
 package com.konai.kurong.faketee.department.dto;
 
+import com.konai.kurong.faketee.employee.utils.DepIdsDto;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
-@Data
-public class DepartmentSaveRequestDto {
+@Getter
+public class DepartmentSaveRequestDto implements DepIdsDto {
 
     @NotBlank
     private String name;
@@ -15,5 +19,18 @@ public class DepartmentSaveRequestDto {
     private Long superId;
     @NotNull
     private List<Long> locationIdList;
+
+    @Override
+    public List<Long> getDepIds() {
+        return getLocationIdList();
+    }
+
+
+//    public DepartmentSaveRequestDto(String name, Long superId, List<Long> locationIdList, List<Long> depIds) {
+//        super(depIds);
+//        this.name = name;
+//        this.superId = superId;
+//        this.locationIdList = locationIdList;
+//    }
 }
 
