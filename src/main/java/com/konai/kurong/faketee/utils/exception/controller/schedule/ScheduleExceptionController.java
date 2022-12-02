265c923f8894f69b0a8f8ec4b27a6eaf94b7942b
hyunseung yang <dev.hsyang@gmail.com>
2022-12-02 16:10:39 +0900

    [FIXED] schedule type 삭제시 참조하고 있는 template 있을 경우 custom error 추가

---
+++ src/main/java/com/konai/kurong/faketee/utils/exception/controller/schedule/ScheduleExceptionController.java
@@ -0,0 +1,8 @@
+package com.konai.kurong.faketee.utils.exception.controller.schedule;
+
+import com.konai.kurong.faketee.utils.exception.controller.ExceptionController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice(basePackages = "com.konai.kurong.faketee.schedule")
+public class ScheduleExceptionController extends ExceptionController {
+}
