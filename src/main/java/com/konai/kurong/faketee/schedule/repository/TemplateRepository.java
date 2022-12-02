c2963e4e629cc8a412d41c1153ff62de2e6f47b4
hyunseung yang <dev.hsyang@gmail.com>
2022-12-02 13:33:58 +0900

    [ADD] schedule info

--- src/main/java/com/konai/kurong/faketee/schedule/repository/TemplateRepository.java
+++ src/main/java/com/konai/kurong/faketee/schedule/repository/TemplateRepository.java
@@ -3,9 +3,10 @@ package com.konai.kurong.faketee.schedule.repository;
 import com.konai.kurong.faketee.schedule.entity.Template;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface TemplateRepository extends JpaRepository<Template, Long> {
     Long countTemplateByScheduleTypeId(Long typeId);
+    List<Template> findTemplatesByScheduleTypeCorporationId(Long corId);
 
 }
--- src/main/java/com/konai/kurong/faketee/schedule/service/TemplateService.java
+++ src/main/java/com/konai/kurong/faketee/schedule/service/TemplateService.java
@@ -1,11 +1,14 @@
 package com.konai.kurong.faketee.schedule.service;
 
+import com.konai.kurong.faketee.schedule.dto.TemplateResponseDto;
 import com.konai.kurong.faketee.schedule.dto.TemplateSaveRequestDto;
 import com.konai.kurong.faketee.schedule.entity.Template;
 import com.konai.kurong.faketee.schedule.repository.TemplateRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -24,5 +27,13 @@ public class TemplateService {
     public void delete(Long id){
 
         templateRepository.deleteById(id);
+    }
+
+    public List<TemplateResponseDto> getTempListByCorId(Long corId){
+        List<Template> templates = templateRepository.findTemplatesByScheduleTypeCorporationId(corId);
+        if(templates.isEmpty()){
+            return null;
+        }
+        return TemplateResponseDto.convertToDtoList(templates);
     }
 }
