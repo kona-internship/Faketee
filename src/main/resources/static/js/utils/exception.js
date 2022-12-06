89aa1b33461528dceefc04909d36b2eb27e60e92
Janghyunho977 <54993215+Janghyunho977@users.noreply.github.com>
2022-12-06 12:47:46 +0900

    [FIXED] exception js

--- src/main/resources/static/js/utils/exception.js
+++ src/main/resources/static/js/utils/exception.js
@@ -1,17 +1,38 @@
 function getErrors(jqXHR) {
     console.log(jqXHR);
-    let jsonValue = jQuery.parseJSON(jqXHR.responseText);
-    return jsonValue.errors;
+    if(jqXHR.responseText != null) {
+        let jsonValue = jQuery.parseJSON(jqXHR.responseText);
+        if(jsonValue.errors != null) {
+            return jsonValue.errors;
+        }
+    }
+    return null;
 }
 
 function showError(errorList){
-    for(let error of errorList){
-        matchId(error.message);
+    if(errorList != null) {
+        for (let error of errorList) {
+            matchId(error.message);
+        }
+    }else{
+        matchId(errorList);
     }
 }
 
 function matchId(id){
     if(id=="400_002_001") {
         alert("하위조직이 존재하여 삭제할 수 없습니다.");
+    }
+    else if(id=="400_003_001"){
+        alert("본인이 속해있는 회사에서의 요청이 아닙니다. 정해진 경로를 통해 다시 한번 접속해주세요.");
+    }
+    else if(id=="400_003_002"){
+        alert("해당 요청에 대한 권한이 없습니다. 조직의 관리자에게 문의바랍니다.");
+    }
+    else if(id=="400_003_003"){
+        alert("본인이 관리하지 않는 직원에 대한 요청이 있습니다. 요청을 다시 한번 확인해주세요.");
+    }
+    else{
+        alert("알 수 없는 오류입니다");
     }
 }
