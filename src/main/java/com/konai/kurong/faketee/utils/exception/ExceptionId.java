63f0a6fa465f8f3f17468d8f769310aa0eb9b083
Janghyunho977 <54993215+Janghyunho977@users.noreply.github.com>
2022-12-06 12:47:14 +0900

    [ADD] exception (empAuth 추가)

--- src/main/java/com/konai/kurong/faketee/utils/exception/ExceptionId.java
+++ src/main/java/com/konai/kurong/faketee/utils/exception/ExceptionId.java
@@ -7,7 +7,13 @@ public enum ExceptionId {
     EMAIL_NOT_FOUND("400_001_002"),
 
 //    ===============DEP=================
-    DEP_ALREADY_EXIST("400_002_001");
+    DEP_ALREADY_EXIST("400_002_001"),
+
+//    ===============EMP=================
+    EMP_AUTH_COR_DIFF("400_003_001"),
+    EMP_AUTH_NOT_PERMIT("400_003_002"),
+    EMP_AUTH_DEP_DIFF("400_003_003");
+
 
 
 
---
+++ src/main/java/com/konai/kurong/faketee/utils/exception/custom/employee/EmpCorDiffException.java
@@ -0,0 +1,11 @@
+package com.konai.kurong.faketee.utils.exception.custom.employee;
+
+import com.konai.kurong.faketee.utils.exception.ExceptionId;
+import com.konai.kurong.faketee.utils.exception.custom.CustomRuntimeException;
+
+public class EmpCorDiffException extends CustomRuntimeException {
+
+    public EmpCorDiffException(){
+        super(ExceptionId.EMP_AUTH_COR_DIFF.getExceptionId());
+    }
+}
