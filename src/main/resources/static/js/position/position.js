8076a09c15acad4cdc6ffbe69c7fbe9e82943a71
hyunseung yang <dev.hsyang@gmail.com>
2022-12-07 16:47:06 +0900

    [FIXED] position html js (list show 변경)

--- src/main/resources/static/js/position/position.js
+++ src/main/resources/static/js/position/position.js
@@ -1,31 +1,71 @@
+let posCheckIdList=[];
+
+function addPosCheckId(id){
+    posCheckIdList.push(id);
+}
+
+function getPosCheckIdList(){
+    return posCheckIdList;
+}
+
+function clearPosCheckIdList(){
+    posCheckIdList=[];
+}
+
 init();
 
 function init() {
-    loadPosList()
+    loadPosList();
 }
 
-function loadPosList() {
+function loadPosList(type) {
     $.ajax({
         url: URL_API_COR_PREFIX + getNextPath(window.location.href, PATH_COR) + "/pos/list",
         type: "GET",
         contentType: "application/json",
         dataType: "json",
         success: function (data) {
-            showPosList(data);
+            showPosList(data, type);
         },
-        error: function () {
+        error: function (jqXHR) {
             alert('직무 목록 불러오기 실패!');
+            let result = getErrors(jqXHR);
+            showError(result);
         }
     });
 }
 
-function showPosList(posList) {
+function showPosList(posList, type) {
     $("#pos-list *").remove();
 
+    switch (type) {
+        case "radio":
+            showRadioPosList(posList);
+            break;
+        case "text":
+        default:
+            showTextPosList(posList);
+            break;
+    }
+}
+
+function showTextPosList(posList){
     for (let [index, pos] of posList.entries()) {
         let msg = '<div>' + (index + 1) + '. 직무명: ' + pos.name
             + ' <button type="button" onclick=goUpdatePos(' + pos.id + ',"' + pos.name + '")>수정</button>'
             + ' <button type="button" onclick=deletePos(' + pos.id + ')>삭제</button>' + '</div>'
+        $('#pos-list').append(msg);
+    }
+}
+
+function showRadioPosList(posList){
+    for (let pos of posList) {
+        let check;
+        if(posCheckIdList.length != 0) {
+            check = posCheckIdList.includes(pos.id);
+        }
+        let msg;
+        msg = '<input type="radio" name="pos" value=' + pos.id + ((check)? ' checked' : '') + '>' + pos.name + '<br>';
         $('#pos-list').append(msg);
     }
 }
@@ -46,8 +86,10 @@ function deletePos(posId) {
             showPosList(data);
 
         },
-        error: function () {
+        error: function (jqXHR) {
             alert('직무 목록 불러오기 실패!');
+            let result = getErrors(jqXHR);
+            showError(result);
         }
     });
 }
@@ -65,8 +107,10 @@ function registerPos() {
             loadPosList();
             alert('등록 성공!');
         },
-        error: function () {
+        error: function (jqXHR) {
             alert('등록 실패!');
+            let result = getErrors(jqXHR);
+            showError(result);
         }
     });
 }
@@ -95,8 +139,10 @@ function updatePos(posId) {
                 alert('수정 성공!');
                 goListPosRegister();
             },
-            error: function () {
+            error: function (jqXHR) {
                 alert('수정 실패!');
+                let result = getErrors(jqXHR);
+                showError(result);
             }
         });
     }
