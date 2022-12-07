6e83c1e2e635b15c1929b75401872c1f00068978
hyunseung yang <dev.hsyang@gmail.com>
2022-12-07 17:30:10 +0900

    [FiXED] corporation (회사생성과 함께 최고관리자 생성 추가)

--- src/main/java/com/konai/kurong/faketee/corporation/service/CorporationService.java
+++ src/main/java/com/konai/kurong/faketee/corporation/service/CorporationService.java
@@ -1,8 +1,19 @@
 package com.konai.kurong.faketee.corporation.service;
 
+import com.konai.kurong.faketee.account.entity.User;
+import com.konai.kurong.faketee.account.repository.UserRepository;
 import com.konai.kurong.faketee.corporation.dto.CorporationSaveRequestDto;
 import com.konai.kurong.faketee.corporation.entity.Corporation;
 import com.konai.kurong.faketee.corporation.repository.CorporationRepository;
+import com.konai.kurong.faketee.department.entity.Department;
+import com.konai.kurong.faketee.department.repository.DepartmentRepository;
+import com.konai.kurong.faketee.employee.entity.Employee;
+import com.konai.kurong.faketee.employee.entity.EmployeeInfo;
+import com.konai.kurong.faketee.employee.repository.EmployeeInfoRepository;
+import com.konai.kurong.faketee.employee.repository.EmployeeRepository;
+import com.konai.kurong.faketee.employee.utils.EmpRole;
+import com.konai.kurong.faketee.position.entity.Position;
+import com.konai.kurong.faketee.position.repository.PositionRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,6 +24,11 @@ import org.springframework.transaction.annotation.Transactional;
 @Slf4j
 public class CorporationService {
     private final CorporationRepository corporationRepository;
+    private final EmployeeRepository employeeRepository;
+    private final UserRepository userRepository;
+    private final DepartmentRepository departmentRepository;
+    private final PositionRepository positionRepository;
+    private final EmployeeInfoRepository employeeInfoRepository;
 
     /**
      * 회사 생성
@@ -22,12 +38,40 @@ public class CorporationService {
      * @return 생성된 회사의 id
      */
     @Transactional
-    public Long registerCorporation(CorporationSaveRequestDto requestDto) {
+    public Long registerCorporation(CorporationSaveRequestDto requestDto,
+                                    Long userId,
+                                    String userName) {
         Corporation cor = requestDto.toEntity();
-//        Users user = (Users) auth.getPrincipal();
-
         Corporation savedCor = corporationRepository.save(cor);
-        /*회사 생성하면서 직원도 생성해야하는뎅ㅇ*/
+
+        User user = userRepository.findById(userId).orElseThrow(()->new IllegalArgumentException());
+        Department department = Department.builder()
+                .name("인사팀")
+                .level(0L)
+                .corporation(savedCor)
+                .build();
+        Department saveDep = departmentRepository.save(department);
+        Position position = Position.builder()
+                .name("CEO")
+                .corporation(savedCor)
+                .build();
+        Position savePos = positionRepository.save(position);
+        EmployeeInfo employeeInfo = EmployeeInfo.builder()
+                .build();
+        EmployeeInfo saveEmp = employeeInfoRepository.save(employeeInfo);
+
+        Employee employee = Employee.builder()
+                .user(user)
+                .department(saveDep)
+                .name(userName)
+                .role(EmpRole.ADMIN)
+                .val("T")
+                .employeeInfo(saveEmp)
+                .corporation(savedCor)
+                .position(savePos)
+                .build();
+        employeeRepository.save(employee);
+
         return savedCor.getId();
     }
 }
