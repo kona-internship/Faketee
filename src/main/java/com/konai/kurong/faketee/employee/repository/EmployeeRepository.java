70107600055e45478c5f20fe0b2b33a9cc72fd43
hyunseung yang <dev.hsyang@gmail.com>
2022-12-08 16:12:51 +0900

    [FIXED] employee repository
    user id로 직원 찾기 추가

--- src/main/java/com/konai/kurong/faketee/employee/repository/EmployeeRepository.java
+++ src/main/java/com/konai/kurong/faketee/employee/repository/EmployeeRepository.java
@@ -3,6 +3,9 @@ package com.konai.kurong.faketee.employee.repository;
 import com.konai.kurong.faketee.employee.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long>, QuerydslEmpRepository {
+    Optional<Employee> findByUserId(Long userId);
 }
