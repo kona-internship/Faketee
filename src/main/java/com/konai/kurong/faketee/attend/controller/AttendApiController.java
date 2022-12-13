13bc0237fc9b00c7eb5f24a69e89087f41d55795
hyunseung yang <dev.hsyang@gmail.com>
2022-12-13 13:39:44 +0900

    [ADD] attend, schedule date request/ annotation 추가

--- src/main/java/com/konai/kurong/faketee/attend/controller/AttendApiController.java
+++ src/main/java/com/konai/kurong/faketee/attend/controller/AttendApiController.java
@@ -1,4 +1,11 @@
 package com.konai.kurong.faketee.attend.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@Slf4j
 public class AttendApiController {
 }
