d5b10d5d9b9d89aca0e40254df39598c5abc36b8
rnkwg <87631304+rnkwg@users.noreply.github.com>
2022-12-13 12:09:06 +0900

    [ADD]employeeApi 권한 설정

--- src/main/java/com/konai/kurong/faketee/employee/dto/EmployeeUpdateRequestDto.java
+++ src/main/java/com/konai/kurong/faketee/employee/dto/EmployeeUpdateRequestDto.java
@@ -1,6 +1,9 @@
 package com.konai.kurong.faketee.employee.dto;
 
+import com.konai.kurong.faketee.department.entity.Department;
+import com.konai.kurong.faketee.department.service.DepartmentService;
 import com.konai.kurong.faketee.employee.entity.EmployeeInfo;
+import com.konai.kurong.faketee.employee.utils.DepIdsDto;
 import com.konai.kurong.faketee.employee.utils.EmpRole;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +12,15 @@ import org.springframework.format.annotation.DateTimeFormat;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 @NoArgsConstructor
 @Data
-public class EmployeeUpdateRequestDto {
+public class EmployeeUpdateRequestDto implements DepIdsDto {
+    private static DepartmentService departmentService;
+
     @NotBlank
     private String name;
 
@@ -36,6 +43,17 @@ public class EmployeeUpdateRequestDto {
     private String major;
     private String cert;
     private String info;
+
+    @Override
+    public List<Long> getDepIds() {
+        Department department = departmentService.findDepartmentById(departmentId);
+        List<Department> depList = departmentService.getSubDepList(department);
+        List<Long> depIdList = new ArrayList<>();
+        for(Department dep : depList) {
+            depIdList.add(dep.getId());
+        }
+        return depIdList;
+    }
 
 //    public EmployeeInfo toEmployeeInfoEntity() {
 //
