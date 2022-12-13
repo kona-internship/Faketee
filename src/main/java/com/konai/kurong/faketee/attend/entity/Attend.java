4218e8039fbd16559c8b6113e84b63d80823b19d
rnkwg <87631304+rnkwg@users.noreply.github.com>
2022-12-13 12:36:14 +0900

    [ADD]ATD Entity

---
+++ src/main/java/com/konai/kurong/faketee/attend/entity/Attend.java
@@ -0,0 +1,56 @@
+package com.konai.kurong.faketee.attend.entity;
+
+import com.konai.kurong.faketee.employee.entity.Employee;
+import com.konai.kurong.faketee.schedule.entity.ScheduleInfo;
+import com.konai.kurong.faketee.utils.jpa_auditing.BaseEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity
+@Getter
+@Builder
+@AllArgsConstructor()
+@NoArgsConstructor()
+@SequenceGenerator(
+        name="ATD_ID_GENERATOR", //시퀀스 제너레이터 이름
+        sequenceName="ATD_SEQUENCE", //시퀀스 이름
+        initialValue=1, //시작값
+        allocationSize=1 //메모리를 통해 할당할 범위 사이즈
+)
+@Table(name = "ATD")
+public class Attend extends BaseEntity {
+    @Id
+    @GeneratedValue(
+            strategy = SEQUENCE,
+            generator = "ATD_ID_GENERATOR"
+    )
+    private Long id;
+
+    @Column(name = "START_TIME")
+    private LocalTime startTime;
+
+    @Column(name = "END_TIME")
+    private LocalTime endTime;
+
+    private String val;
+
+    private LocalDateTime date;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "SCH_INFO_ID")
+    private ScheduleInfo scheduleInfo;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "EMP_ID")
+    private Employee employee;
+}
