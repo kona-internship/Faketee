070f9d0bc02767dc11687b7f9ce4dc907e082cb4
Janghyunho977 <54993215+Janghyunho977@users.noreply.github.com>
2022-12-15 09:53:47 +0900

    [ADD] draft repository (querydsl 추가)

---
+++ src/main/java/com/konai/kurong/faketee/draft/repository/QuerydslDraftRepository.java
@@ -0,0 +1,12 @@
+package com.konai.kurong.faketee.draft.repository;
+
+import com.konai.kurong.faketee.draft.entity.Draft;
+import com.konai.kurong.faketee.draft.utils.DraftStateCode;
+
+import java.util.List;
+
+public interface QuerydslDraftRepository {
+    List<Draft> getDraftsWithRequestsByApproverAndStateCode(Long apvlEmpId, List<DraftStateCode> draftStateCodeList);
+    List<Draft> getDraftsWithRequestsByEmployeeAndStateCode(Long empId, List<DraftStateCode> draftStateCodeList);
+    void updateDraftStateCode(Long draftId, DraftStateCode draftStateCode);
+}
