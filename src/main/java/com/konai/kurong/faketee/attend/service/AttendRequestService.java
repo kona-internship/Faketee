e6cc29004affbec21beeccb90ee50d06a4823a46
rnkwg <87631304+rnkwg@users.noreply.github.com>
2022-12-15 12:21:50 +0900

    [ADD]AttendRequest SAVE

--- src/main/java/com/konai/kurong/faketee/attend/service/AttendRequestService.java
+++ src/main/java/com/konai/kurong/faketee/attend/service/AttendRequestService.java
@@ -8,6 +8,9 @@ import com.konai.kurong.faketee.attend.repository.AttendRequestRepository;
 import com.konai.kurong.faketee.attend.utils.AttendRequestVal;
 import com.konai.kurong.faketee.draft.entity.Draft;
 import com.konai.kurong.faketee.draft.repository.DraftRepository;
+import com.konai.kurong.faketee.draft.utils.DraftCrudType;
+import com.konai.kurong.faketee.draft.utils.DraftRequestType;
+import com.konai.kurong.faketee.draft.utils.DraftStateCode;
 import com.konai.kurong.faketee.employee.entity.Employee;
 import com.konai.kurong.faketee.employee.repository.EmployeeRepository;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +19,7 @@ import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.sql.Date;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 @Slf4j
@@ -31,25 +35,46 @@ public class AttendRequestService {
     //출퇴근기록 생성 요청
     @Transactional
     public void createAttendRequest(AttendRequestSaveDto requestDto) {
-//        출퇴근기록 요청(ATD_REQ) 만들기에 필요한 것들 불러오기
-        AttendRequestVal val = AttendRequestVal.C;
-        LocalTime startTime = LocalTime.parse(requestDto.getStartTime());
-        LocalTime endTime = LocalTime.parse(requestDto.getEndTime());
-        Employee employee = employeeRepository.findById(requestDto.getEmpId()).orElseThrow();
-        Draft draft = draftRepository.findById(requestDto.getDraftId()).orElseThrow();
 
-//        출퇴근기록 요청(ATD_REQ) Entity 만들어서 먼저 저장하기
-        AttendRequest attendRequest = AttendRequest.builder()
-                .date(new Date(requestDto.getDate().getTime()).toLocalDate())
-                .val(val.getVal())
-                .startTime(startTime)
-                .endTime(endTime)
-                .employee(employee)
-                .draft(draft)
+//        기안(DRAFT) 만들기에 필요한 것들 불러오기
+        LocalDateTime requestDate = LocalDateTime.now();
+        DraftStateCode draftStateCode = DraftStateCode.WAIT;
+        DraftRequestType draftRequestType = DraftRequestType.ATTENDANCE;
+        DraftCrudType draftCrudType = DraftCrudType.CREATE;
+        Employee requestEmployee = employeeRepository.findById(requestDto.getReqEmpId()).orElseThrow();
+
+//        기안(DRAFT) Entity 만들기
+        Draft draft = Draft.builder()
+                .approvalDate(null)     //  나중에 승인권자 승인하면 승인 날짜 넣어주기
+                .requestDate(requestDate)
+                .approvalMessage(null)     //   나중에 승인권자 승인하면 승인권자 메세지 넣어주기
+                .stateCode(draftStateCode)
+                .requestMessage(requestDto.getRequestMessage())
+                .requestType(draftRequestType)
+                .crudType(draftCrudType)
+                .requestEmployee(requestEmployee)
                 .build();
 
+
+//        출퇴근기록 요청(ATD_REQ) 만들기에 필요한 것들 불러오기
+//        AttendRequestVal val = AttendRequestVal.C;
+//        LocalTime startTime = LocalTime.parse(requestDto.getStartTime());
+//        LocalTime endTime = LocalTime.parse(requestDto.getEndTime());
+//        Employee employee = employeeRepository.findById(requestDto.getReqEmpId()).orElseThrow();
+//        Draft draft = draftRepository.findById(requestDto.getDraftId()).orElseThrow();
+//
+////        출퇴근기록 요청(ATD_REQ) Entity 만들기
+//        AttendRequest attendRequest = AttendRequest.builder()
+//                .date(new Date(requestDto.getDate().getTime()).toLocalDate())
+//                .val(val.getVal())
+//                .startTime(startTime)
+//                .endTime(endTime)
+//                .employee(employee)
+//                .draft(draft)
+//                .build();
+
 //        출퇴근기록 요청(ATD_REQ) Entity 저장하기
-        attendRequestRepository.save(attendRequest);
+//        attendRequestRepository.save(attendRequest);
 
 //        ######근태 테이블에 해당 날짜에 해당 출퇴근 기록 관련된 정보를 저장해줘야 함######
     }
