e5fa0182122aab861805c19f342b16af620683b2
sh_park <shparkso162@gmail.com>
2022-12-16 13:59:42 +0900

    [ADD] 근태 dto

--- src/main/java/com/konai/kurong/faketee/attend/dto/AttendResponseDto.java
+++ src/main/java/com/konai/kurong/faketee/attend/dto/AttendResponseDto.java
@@ -6,8 +6,14 @@ import lombok.Data;
 @Data
 @Builder
 public class AttendResponseDto {
-    private String startTime;
-    private String endTime;
+    private String schStartTime;
+    private String schEndTime;
     private String depName;
+    //근무중등등
+    private String state;
+    private String atdStartTime;
+    private String atdEndTime;
+    //오전반차, 일반근무, 등등
+    private String todaySch;
 
 }
