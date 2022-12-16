3b6364a5afc4b7db9d3c8494fa4ef640d882d60f
hyunseung yang <dev.hsyang@gmail.com>
2022-12-16 13:32:57 +0900

    [FIXED] 로그인 세션에 유저 EMP 정보 추가, dto수정, empId 찾는 로직 수정

--- src/main/java/com/konai/kurong/faketee/auth/dto/SessionUser.java
+++ src/main/java/com/konai/kurong/faketee/auth/dto/SessionUser.java
@@ -4,11 +4,13 @@ import com.konai.kurong.faketee.account.entity.User;
 import com.konai.kurong.faketee.account.util.Role;
 import com.konai.kurong.faketee.account.util.Type;
 import com.konai.kurong.faketee.employee.dto.EmployeeResponseDto;
+import com.konai.kurong.faketee.employee.dto.EmployeeSessionResponseDto;
 import com.konai.kurong.faketee.employee.entity.Employee;
 import lombok.Getter;
 import org.springframework.security.core.SpringSecurityCoreVersion;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -22,7 +24,7 @@ public class SessionUser implements Serializable {
     private String name;
     private Role role;
     private Type type;
-    private List<EmployeeResponseDto> employeeList;
+    private List<EmployeeSessionResponseDto> employeeList = new ArrayList<>();
 
     public SessionUser(User user){
 
@@ -34,10 +36,8 @@ public class SessionUser implements Serializable {
         this.type = user.getType();
     }
 
-    public void setEmployeeIdList(List<EmployeeResponseDto> employeeList){
+    public void setEmployeeIdList(List<EmployeeSessionResponseDto> employeeList){
 
-        for(EmployeeResponseDto employee : employeeList){
-            employeeList.add(employee);
-        }
+        this.employeeList.addAll(employeeList);
     }
 }
