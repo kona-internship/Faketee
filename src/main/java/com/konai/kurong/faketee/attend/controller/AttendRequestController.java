8d7b5ebdc7f715cdc6ef4b63c43234841de6c831
hyunseung yang <dev.hsyang@gmail.com>
2022-12-19 09:05:40 +0900

    [ADD]AttendRequest Controller

--- src/main/java/com/konai/kurong/faketee/attend/controller/AttendRequestController.java
+++ src/main/java/com/konai/kurong/faketee/attend/controller/AttendRequestController.java
@@ -1,4 +1,28 @@
 package com.konai.kurong.faketee.attend.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/corporation/{corId}/atd/req")
 public class AttendRequestController {
+    @GetMapping("/create")
+    public String create() {
+        return "attend/request/create";
+    }
+
+    @GetMapping("/update")
+    public String update() {
+        return "attend/request/update";
+    }
+
+    @GetMapping("/delete")
+    public String delete() {
+        return "attend/request/delete";
+    }
 }
