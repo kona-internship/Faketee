f91e638dfdb6ee4d4ac5f39174e12cfbbd574e4a
rnkwg <87631304+rnkwg@users.noreply.github.com>
2022-12-19 14:42:58 +0900

    [ADD]AttendRequest Controller

--- src/main/java/com/konai/kurong/faketee/attend/controller/AttendRequestApiController.java
+++ src/main/java/com/konai/kurong/faketee/attend/controller/AttendRequestApiController.java
@@ -1,4 +1,37 @@
 package com.konai.kurong.faketee.attend.controller;
 
+import com.konai.kurong.faketee.attend.service.AttendRequestService;
+import com.konai.kurong.faketee.auth.LoginUser;
+import com.konai.kurong.faketee.auth.dto.SessionUser;
+import com.konai.kurong.faketee.schedule.dto.ScheduleInfoResponseDto;
+import com.konai.kurong.faketee.schedule.service.ScheduleInfoService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDate;
+
+@RequiredArgsConstructor
+@RestController
+@Slf4j
+@RequestMapping("/api/corporation/{corId}/atd/req")
 public class AttendRequestApiController {
+
+    private final AttendRequestService attendRequestService;
+
+    private final ScheduleInfoService scheduleInfoService;
+
+    @GetMapping("/create/check-date")
+    public ResponseEntity<?> createAtdRecord(LocalDate date) {
+        return ResponseEntity.ok(attendRequestService.checkAtdRecord(date));
+    }
+
+    @GetMapping("/create/set-time/sch-info")
+    public ResponseEntity<?> createSetTimeSchInfo(String date,
+                                                  @PathVariable(name = "corId") Long corId,
+                                                  @LoginUser SessionUser user) {
+        return new ResponseEntity<>(ScheduleInfoResponseDto.convertToDto(scheduleInfoService.getSchByDateAndEmp(date, corId, user.getId())), HttpStatus.OK);
+    }
 }
--- src/main/java/com/konai/kurong/faketee/attend/controller/AttendRequestController.java
+++ src/main/java/com/konai/kurong/faketee/attend/controller/AttendRequestController.java
@@ -3,8 +3,14 @@ package com.konai.kurong.faketee.attend.controller;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDate;
+import java.util.Date;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -14,6 +20,23 @@ public class AttendRequestController {
     @GetMapping("/create")
     public String create() {
         return "attend/request/create";
+    }
+
+    @GetMapping("/create/set-time")
+    public String createSetTime(Model model, @RequestParam String selectedDate) {
+        model.addAttribute("selectedDate", selectedDate);
+        return "attend/request/create-setTime";
+    }
+
+    @GetMapping("/create/set-apvl")
+    public String createSetApvl(Model model, @RequestParam String selectedDate, @RequestParam String startTime,
+                                @RequestParam String endTime, @RequestParam Long schInfoId, @RequestParam String schInfo) {
+        model.addAttribute("selectedDate", selectedDate);
+        model.addAttribute("startTime", startTime);
+        model.addAttribute("endTime", endTime);
+        model.addAttribute("schInfoId", schInfoId);
+        model.addAttribute("schInfo", schInfo);
+        return "attend/request/create-setApvl";
     }
 
     @GetMapping("/update")
