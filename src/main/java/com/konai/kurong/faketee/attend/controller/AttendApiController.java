ae48660e1934f5576f75a860ad5db27545f5225b
hyunseung yang <dev.hsyang@gmail.com>
2022-12-20 14:51:19 +0900

    [FIXED] add @EmpAuth

--- src/main/java/com/konai/kurong/faketee/attend/controller/AttendApiController.java
+++ src/main/java/com/konai/kurong/faketee/attend/controller/AttendApiController.java
@@ -3,6 +3,8 @@ package com.konai.kurong.faketee.attend.controller;
 import com.konai.kurong.faketee.attend.service.AttendService;
 import com.konai.kurong.faketee.auth.LoginUser;
 import com.konai.kurong.faketee.auth.dto.SessionUser;
+import com.konai.kurong.faketee.employee.utils.EmpAuth;
+import com.konai.kurong.faketee.employee.utils.EmpRole;
 import com.konai.kurong.faketee.employee.utils.ReqEmp;
 import com.konai.kurong.faketee.employee.utils.ReqEmpInfo;
 import com.konai.kurong.faketee.location.service.LocationService;
@@ -20,9 +22,11 @@ public class AttendApiController {
     private final AttendService attendService;
     private final LocationService locationService;
 
+    @EmpAuth(role = EmpRole.EMPLOYEE)
     @GetMapping("/load")
     public ResponseEntity<?> loadHome(@PathVariable(name = "corId") Long corId,
                                       @RequestParam("date") String date, @ReqEmp ReqEmpInfo empInfo) {
+        log.info("dsd");
 
         return new ResponseEntity<>(attendService.getUserScheduleInfo(corId, date, empInfo.getId()), HttpStatus.OK);
     }
@@ -32,10 +36,11 @@ public class AttendApiController {
         return new ResponseEntity<>(locationService.getAtdLocList(corId, user.getId()), HttpStatus.OK);
     }
 
+    @EmpAuth(role = EmpRole.EMPLOYEE)
     @GetMapping("/reg")
     public ResponseEntity<?> clickAtd(@PathVariable(name = "corId") Long corId,
                                          @RequestParam("onoff") String onOff,
-                                      @ReqEmp ReqEmpInfo empInfo) throws Exception {
+                                      @ReqEmp ReqEmpInfo empInfo){
 
         attendService.clickAtd(corId, empInfo.getId(), onOff);
 
