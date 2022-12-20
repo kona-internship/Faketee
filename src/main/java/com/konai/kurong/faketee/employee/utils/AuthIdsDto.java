adeece0870356a1e4b79dd2ed9ff789c22a8593e
Janghyunho977 <54993215+Janghyunho977@users.noreply.github.com>
2022-12-20 14:41:01 +0900

    [REFACTOR] empAuth (EmpAuthParamType 타입 추가)

--- src/main/java/com/konai/kurong/faketee/employee/utils/AuthIdsDto.java
+++ src/main/java/com/konai/kurong/faketee/employee/utils/AuthIdsDto.java
@@ -1,9 +1,10 @@
 package com.konai.kurong.faketee.employee.utils;
 
 /**
- * 조직관리자의 권한을 갖는 메서드에 대해서 자신의 하위조직인지 검증이 필요할 때 구현해주는 인터페이스
+ * 어노테이션 @EmpAuth 의 타입(paramCheckType)에 따라 검증할 목록을 반환
+ * 검증받을 목록은 EmpAuthCheckList 클래스에 래핑하여 반환한다.
  *
- * getDepIds(): 요청자의 하위 조직임을 검증 받을 조직들의 리스트
+ * getEmpAuthCheckList(): 요청자의 하위 조직임을 검증 받을 목록 객체
  */
 public interface AuthIdsDto {
     EmpAuthCheckList getEmpAuthCheckList();
