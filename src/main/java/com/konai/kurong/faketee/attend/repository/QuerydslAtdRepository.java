392d31fdc531150e60c5ca6118c0bba6c2889969
rnkwg <87631304+rnkwg@users.noreply.github.com>
2022-12-21 12:53:11 +0900

    [ADD]달의 출퇴근기록 가져오기

--- src/main/java/com/konai/kurong/faketee/attend/repository/QuerydslAtdRepository.java
+++ src/main/java/com/konai/kurong/faketee/attend/repository/QuerydslAtdRepository.java
@@ -2,8 +2,12 @@ package com.konai.kurong.faketee.attend.repository;
 
 import com.konai.kurong.faketee.attend.entity.Attend;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.List;
 
 public interface QuerydslAtdRepository {
     void updateAtdEndTime(Long scheInfoId, LocalTime endTime);
+
+    List<Attend> getAttendByMonth(LocalDate startDate, LocalDate lastDate);
 }
