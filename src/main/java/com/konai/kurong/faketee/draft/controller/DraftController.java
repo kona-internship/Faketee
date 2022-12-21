5f5bba1f63f5f30d6aaf4faf9bae310885db0d0a
hyunseung yang <dev.hsyang@gmail.com>
2022-12-21 12:31:39 +0900

    [FIXED] draft (목록 가져오기, 승인, 삭제 기능 추가)

--- src/main/java/com/konai/kurong/faketee/draft/controller/DraftController.java
+++ src/main/java/com/konai/kurong/faketee/draft/controller/DraftController.java
@@ -1,9 +1,12 @@
 package com.konai.kurong.faketee.draft.controller;
 
+import com.konai.kurong.faketee.draft.service.DraftService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
@@ -12,8 +15,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/corporation/{corId}/draft")
 public class DraftController {
 
+    private final DraftService draftService;
+
     @GetMapping("/apvl-list")
-    public String registerEmp() {
+    public String apvlWaitList() {
         return "draft/apvlWaitList";
+    }
+
+    @GetMapping("/done-list")
+    public String reqDoneList() {
+        return "draft/reqDoneList";
+    }
+
+    @GetMapping("/req-list")
+    public String reqList() {
+        return "draft/reqList";
+    }
+
+    @GetMapping("/req/{draftId}")
+    public String getReqDetail(@PathVariable("draftId") Long draftId, Model model){
+        model.addAttribute("draft", draftService.getDraft(draftId));
+        return "draft/reqDetail";
+    }
+
+    @GetMapping("/apvl/{draftId}")
+    public String getApvlDetail(@PathVariable("draftId") Long draftId, Model model){
+        model.addAttribute("draft", draftService.getDraft(draftId));
+        return "draft/apvlDetail";
     }
 }
--- src/main/java/com/konai/kurong/faketee/draft/repository/QuerydslDraftRepository.java
+++ src/main/java/com/konai/kurong/faketee/draft/repository/QuerydslDraftRepository.java
@@ -3,6 +3,7 @@ package com.konai.kurong.faketee.draft.repository;
 import com.konai.kurong.faketee.draft.entity.Draft;
 import com.konai.kurong.faketee.draft.utils.DraftStateCode;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface QuerydslDraftRepository {
@@ -10,4 +11,6 @@ public interface QuerydslDraftRepository {
     List<Draft> getDraftsWithRequestsByEmployeeAndStateCode(Long empId, List<DraftStateCode> draftStateCodeList);
     void updateDraftStateCode(Long draftId, DraftStateCode draftStateCode);
     List<Long> getDraftIdsByEmployeeId(Long empId);
+
+    void updateDraftStateCodeAndDateAndMessageByApvlEmp(Long draftId, DraftStateCode draftStateCode, LocalDateTime dateTime, String apvlMsg);
 }
