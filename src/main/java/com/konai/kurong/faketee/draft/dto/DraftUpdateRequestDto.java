87b6446d65e831345c03e0c9154d44fd9203c572
Janghyunho977 <54993215+Janghyunho977@users.noreply.github.com>
2022-12-26 17:30:14 +0900

    [FIXED] draft (버그 수정)
    1. 타임리프 문법 오류
    2. 화면 단 연결 ajax url 수정
    3. 레포지토리 로직 수정
    4. 서비스 컨트롤러 단 수정
    5. 디티오 버그(널포인터) 수정

--- src/main/java/com/konai/kurong/faketee/draft/dto/DraftUpdateRequestDto.java
+++ src/main/java/com/konai/kurong/faketee/draft/dto/DraftUpdateRequestDto.java
@@ -1,12 +1,16 @@
 package com.konai.kurong.faketee.draft.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class DraftUpdateRequestDto {
     @NotNull
     private Long draftId;
